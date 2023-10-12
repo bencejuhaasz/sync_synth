3b5ae0e7cb2f7101daf595a15f21c5c837b53164
bencejuhaasz <32618702+bencejuhaasz@users.noreply.github.com>
2023-10-13 00:55:21 +0200

    Update sync_synth-scripts.js
    One step each beat. Faster doesn't work yet.
    engine.beginTimer is inaccurate

--- sync_synth-scripts.js
+++ sync_synth-scripts.js
@@ -1,20 +1,20 @@
 function SyncSynth() {
   // Define your plugin properties here
-  this.vendor = "Your Vendor Name";
+  this.vendor = "Generic MIDI Device";
   this.name = "Sync Synth";
   this.description = "Sync an external synth to Mixxx's BPM.";
 }
 
 var beatTimer; // Declare the beat timer variable
 var ppqn = 24; // Pulses per quarter note
-
+var midiClockActive = false;
 
 // Initialize the plugin
 SyncSynth.prototype.init = function() {
   // Add initialization code here
   engine.connectControl("[Channel1]", "play", "onPlayStateChange");
-  //engine.connectControl("[Channel1]", "beat_active", "onBeat");
-  beatTimer = engine.beginTimer(1000 / (engine.getValue("[Channel1]", "bpm") / 60 * ppqn), "SyncSynth.onBeat()");
+  engine.connectControl("[Channel1]", "beat_active", "onBeat");
+  
 };
 
 // Handle MIDI input (if needed)
@@ -38,14 +38,25 @@ function onPlayStateChange(value) {
     } else {
         // Channel 1 has stopped, send MIDI Clock Stop (0xFC)
         midi.sendShortMsg(0xFC, 0x00, 0x00);
+        // Reset the MIDI Clock state
         console.log("-------MIDI Stop-------");
     }
 }
 
-SyncSynth.onBeat = function() {
-    // Send MIDI Clock tick (0xF8) on each beat
-    midi.sendShortMsg(0xF8, 0x00, 0x00);
-    console.log("-------MIDI Clock-------");
+function onBeat(value) {
+    // When beat_active control changes, value will be 1 during a beat
+    if (value === 1) {
+        // Send 24 MIDI Clock ticks (0xF8) at once
+        for (var i = 0; i < ppqn; i++) {
+         midi.sendShortMsg(0xF8, 0x00, 0x00);
+        }        
+
+        // Set the flag to prevent sending multiple MIDI Clock messages during a single beat
+        midiClockActive = true;
+    } else if (value === 0) {
+        // Reset the flag at the end of the beat
+        midiClockActive = false;
+    }
 }
 
 function shutdown() {
